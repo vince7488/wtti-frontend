31c70399bc929b3b7b796c579b09878e759af1d6
Vernard Mercader <vmercader@oteemofederal.com>
2024-06-26 09:16:07 -0400

    Config options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+/** @type {import('vite').UserConfig} */
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3333,
+    // open: true,
+  },
+  preview: {
+    port: 7488,
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
